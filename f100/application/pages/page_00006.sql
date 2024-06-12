305676a33dca4f098b4f5c6f2b64826eb89e570c
Martínez-Risco <smartinez@quistor.com>
2024-06-12 16:55:43 +0200

    Nueva página 6

---
+++ f100/application/pages/page_00006.sql
@@ -0,0 +1,133 @@
+prompt --application/pages/page_00006
+begin
+--   Manifest
+--     PAGE: 00006
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2023.10.31'
+,p_release=>'23.2.3'
+,p_default_workspace_id=>7607916612212207
+,p_default_application_id=>101
+,p_default_id_offset=>7217772657612304
+,p_default_owner=>'SANTI_APEX'
+);
+wwv_flow_imp_page.create_page(
+ p_id=>6
+,p_name=>'NEW CHART'
+,p_alias=>'NEW-CHART'
+,p_step_title=>'NEW CHART'
+,p_autocomplete_on_off=>'OFF'
+,p_page_template_options=>'#DEFAULT#'
+,p_protection_level=>'C'
+,p_last_updated_by=>'LIFE_CYCLE'
+,p_last_upd_yyyymmddhh24miss=>'20240612104111'
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(56002684886037884)
+,p_plug_name=>'Params'
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_component_template_options=>'#DEFAULT#'
+,p_escape_on_http_output=>'Y'
+,p_plug_template=>wwv_flow_imp.id(23517304376010544)
+,p_plug_display_sequence=>10
+,p_plug_item_display_point=>'BELOW'
+,p_query_type=>'SQL'
+,p_plug_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select value, name from (',
+'select 2 value, ''param_1'' name, 1 ord from dual',
+'union',
+'select 4 value, ''param_2'' name, 2 ord from dual',
+'union',
+'select 7 value, ''param_7'' name, 3 ord from dual',
+'union',
+'select 3 value, ''param_3'' name, 4 ord from dual',
+') order by ord'))
+,p_plug_source_type=>'NATIVE_JET_CHART'
+);
+wwv_flow_imp_page.create_jet_chart(
+ p_id=>wwv_flow_imp.id(1000327570222344)
+,p_region_id=>wwv_flow_imp.id(56002684886037884)
+,p_chart_type=>'area'
+,p_height=>'400'
+,p_animation_on_display=>'auto'
+,p_animation_on_data_change=>'auto'
+,p_orientation=>'vertical'
+,p_data_cursor=>'auto'
+,p_data_cursor_behavior=>'auto'
+,p_hide_and_show_behavior=>'withRescale'
+,p_hover_behavior=>'dim'
+,p_stack=>'off'
+,p_connect_nulls=>'Y'
+,p_sorting=>'label-asc'
+,p_fill_multi_series_gaps=>true
+,p_zoom_and_scroll=>'off'
+,p_tooltip_rendered=>'Y'
+,p_show_series_name=>true
+,p_show_group_name=>true
+,p_show_value=>true
+,p_legend_rendered=>'on'
+,p_legend_position=>'auto'
+);
+wwv_flow_imp_page.create_jet_chart_series(
+ p_id=>wwv_flow_imp.id(1002088421222351)
+,p_chart_id=>wwv_flow_imp.id(1000327570222344)
+,p_seq=>10
+,p_name=>'param_vals'
+,p_location=>'REGION_SOURCE'
+,p_items_value_column_name=>'VALUE'
+,p_items_label_column_name=>'NAME'
+,p_line_type=>'auto'
+,p_marker_rendered=>'auto'
+,p_marker_shape=>'auto'
+,p_assigned_to_y2=>'off'
+,p_items_label_rendered=>true
+,p_items_label_position=>'auto'
+);
+wwv_flow_imp_page.create_jet_chart_axis(
+ p_id=>wwv_flow_imp.id(1000895968222347)
+,p_chart_id=>wwv_flow_imp.id(1000327570222344)
+,p_axis=>'x'
+,p_is_rendered=>'on'
+,p_format_scaling=>'auto'
+,p_scaling=>'linear'
+,p_baseline_scaling=>'zero'
+,p_major_tick_rendered=>'on'
+,p_minor_tick_rendered=>'off'
+,p_tick_label_rendered=>'on'
+,p_tick_label_rotation=>'auto'
+,p_tick_label_position=>'outside'
+,p_zoom_order_seconds=>false
+,p_zoom_order_minutes=>false
+,p_zoom_order_hours=>false
+,p_zoom_order_days=>false
+,p_zoom_order_weeks=>false
+,p_zoom_order_months=>false
+,p_zoom_order_quarters=>false
+,p_zoom_order_years=>false
+);
+wwv_flow_imp_page.create_jet_chart_axis(
+ p_id=>wwv_flow_imp.id(1001519307222349)
+,p_chart_id=>wwv_flow_imp.id(1000327570222344)
+,p_axis=>'y'
+,p_is_rendered=>'on'
+,p_format_type=>'decimal'
+,p_decimal_places=>0
+,p_format_scaling=>'none'
+,p_scaling=>'linear'
+,p_baseline_scaling=>'zero'
+,p_position=>'auto'
+,p_major_tick_rendered=>'on'
+,p_minor_tick_rendered=>'off'
+,p_tick_label_rendered=>'on'
+,p_zoom_order_seconds=>false
+,p_zoom_order_minutes=>false
+,p_zoom_order_hours=>false
+,p_zoom_order_days=>false
+,p_zoom_order_weeks=>false
+,p_zoom_order_months=>false
+,p_zoom_order_quarters=>false
+,p_zoom_order_years=>false
+);
+wwv_flow_imp.component_end;
+end;
+/
